84a082b137d369fbed270f3fdba28d123502d03d
xiaoio4 <iosengineer@163.com>
2015-01-07 14:18:50 +0800

    epub
    添加了解压、XML解析和epub的三方库

---
+++ epubDemo/UIWebView+SearchWebView.h
@@ -0,0 +1,9 @@
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+@interface UIWebView (SearchWebView)
+
+- (NSInteger)highlightAllOccurencesOfString:(NSString*)str;
+- (void)removeAllHighlights;
+
+@end
